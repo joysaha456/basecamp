317da3aa4b72c1e768c74382d64d1200d05898aa
MD Kamrul Islam <kamrul1cse@gmail.com>
2020-07-09 15:47:45 +0600

    Updates

---
+++ precache-manifest.a89a3ae6552da5dbb44042e5244cae80.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2fa44a4e8813c68b2ee",
+    "url": "/joysaha456/react-basecamp/static/js/main.c2fa44a4.chunk.js"
+  },
+  {
+    "revision": "675887439d5ceff73eaa",
+    "url": "/joysaha456/react-basecamp/static/css/1.d34e80f0.chunk.css"
+  },
+  {
+    "revision": "675887439d5ceff73eaa",
+    "url": "/joysaha456/react-basecamp/static/js/1.67588743.chunk.js"
+  },
+  {
+    "revision": "ac0fa00275a001212573",
+    "url": "/joysaha456/react-basecamp/static/js/runtime~main.ac0fa002.js"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/joysaha456/react-basecamp/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/joysaha456/react-basecamp/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/joysaha456/react-basecamp/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/joysaha456/react-basecamp/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "1f73c31fc5c9cbe5874f24c5b870b1de",
+    "url": "/joysaha456/react-basecamp/static/media/icon-plus-gray.1f73c31f.svg"
+  },
+  {
+    "revision": "8a422611d0bd4451914900a50d4237d5",
+    "url": "/joysaha456/react-basecamp/static/media/icon-minus-gray.8a422611.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/joysaha456/react-basecamp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/joysaha456/react-basecamp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/joysaha456/react-basecamp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/joysaha456/react-basecamp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/joysaha456/react-basecamp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d6ba67eea73498add4044e1c9eebeb10",
+    "url": "/joysaha456/react-basecamp/index.html"
+  }
+];
